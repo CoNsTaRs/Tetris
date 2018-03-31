5948b3789f8c6a1e442010ce583fe03bcf71380b
CoNsTaRs <constarwu@gmail.com>
2018-03-31 04:24:42 -0400

    Comment updated

--- Playfield.hs
+++ Playfield.hs
@@ -1,12 +1,12 @@
 module Playfield where
 
-  -- List of cells that is taken
+  -- A Playfield is represented by a list of cells that is taken
   type Playfield = [(Int, Int)]
 
-  -- The width of the playfield, left is 0 and right is 9
+  -- The width of the playfield, left to 0 and right to 9
   fieldWidth :: Int
   fieldWidth = 10
 
-  -- The height of the playfield, top is zero and buttom is 19
+  -- The height of the playfield, zero on the top and 19 at the buttom
   fieldHeight :: Int
   fieldHeight = 20
