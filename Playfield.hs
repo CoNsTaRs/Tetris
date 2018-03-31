d453dade519f879ee1e7717ec86edfc19e2908e9
CoNsTaRs <constarwu@gmail.com>
2018-03-30 23:47:46 -0400

    Comments added

--- Playfield.hs
+++ Playfield.hs
@@ -1,9 +1,12 @@
 module Playfield where
-  
+
+  -- List of cells that is taken
   type Playfield = [(Int, Int)]
   
+  -- The width of the playfield
   fieldWidth :: Int
   fieldWidth = 10
 
+  -- The height of the playfield
   fieldHeight :: Int
   fieldHeight = 20
