275e968258d457975b3cf79c92d4f256b41b3941
CoNsTaRs <constarwu@gmail.com>
2018-04-04 01:18:49 -0400

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -1,38 +1,92 @@
 module Main where
 
-  import UI.NCurses
+  import System.IO
   import Tetromino
-  import Tetris
+  import Playfield
   import RotationSystem
   import SuperRS
+  import Tetris
 
-  f :: (Int, Int) -> Update ()
-  f (x, y)
-    | y >= 0 = moveCursor (toInteger y) (toInteger (2 * x)) *> drawString "██"
-    | otherwise = moveCursor 0 0
+  data Action = MVLFT | MVRHT | MVDWN | HRDRP | ROTCW | NOACT
+    deriving (Eq)
 
-  printMino :: Mino -> Update ()
-  printMino (Mino (x : []) _ _) = f x
-  printMino (Mino (x : xs) s r) = f x *> printMino (Mino xs s r)
+  hideCursor :: IO ()
+  hideCursor = putStr "\ESC[?25l"
 
-  printField :: [(Int, Int)] -> Update ()
-  printField (x : []) = f x
-  printField (x : xs) = f x *> printField xs
+  moveCursor :: Int -> Int -> IO ()
+  moveCursor x y  = putStr escapeCode
+    where
+      escapeCode :: String
+      escapeCode = "\ESC[" ++ show (y + 1) ++ ";" ++ show ((x + 1) * 2) ++ "H"
 
-  waitFor :: Window -> (Event -> Bool) -> Curses ()
-  waitFor w p = loop where
-    loop = do
-      ev <- getEvent w Nothing
-      case ev of
-        Nothing -> loop
-        Just ev' -> if p ev' then return () else loop
+  eraseMino :: Mino -> IO ()
+  eraseMino (Mino []            _ _) = return ()
+  eraseMino (Mino ((x, y) : as) r s) = moveCursor x y *> putStr "  " *> eraseMino (Mino as r s)
+
+  printMino :: Mino -> IO ()
+  printMino (Mino []            _ _) = return ()
+  printMino (Mino ((x, y) : as) r s)
+    | x >= 0 && y >= 0 = moveCursor x y *> putStr "██" *> printMino (Mino as r s)
+    | otherwise = return ()
+
+  getKey :: IO String
+  getKey = reverse <$> getKey' ""
+    where
+      getKey' chars = do
+        char <- getChar
+        more <- hReady stdin
+        (if more then getKey' else return) (char : chars)
+
+  getAction :: IO Action
+  getAction = do
+    key <- getKey
+    case key of
+      "\ESC[A" -> return ROTCW
+      "\ESC[B" -> return MVDWN
+      "\ESC[C" -> return MVRHT
+      "\ESC[D" -> return MVLFT
+      "\SP"    -> return HRDRP
+      _        -> return NOACT
+
+  doAction :: RotationSystem -> Playfield -> Mino -> Action -> IO (Playfield, Mino)
+  doAction rs pf mino act
+    | act == MVLFT = let mino'= moveLft pf mino in eraseMino mino *> printMino mino' *> return (pf, mino')
+    | act == MVRHT = let mino'= moveRht pf mino in eraseMino mino *> printMino mino' *> return (pf, mino')
+    | act == MVDWN = let mino'= softDrop pf mino in eraseMino mino *> printMino mino' *> return (pf, mino')
+    -- | act == HRDRP = eraseMino mino *> printMino mino' where mino'= (hardDrop pf mino)
+    | act == ROTCW = let  mino'= rotate rs pf mino CW in eraseMino mino *> printMino mino' *> return (pf, mino')
+    | otherwise    = return (pf, mino)
 
   main :: IO ()
-  main = runCurses $ do
-    setEcho False
-    _ <- setCursorMode CursorInvisible
-    w <- defaultWindow
-    updateWindow w $ do
-      printMino (rotate superRS [] (Mino [(0, -1), (0,  0), (0,  1), (0, 2)] ShpI Rht) CC)
-    render
-    waitFor w (\ev -> ev == EventCharacter 'q' || ev == EventCharacter 'Q')
+  main = do
+    hideCursor
+    hSetEcho stdin False
+    hSetBuffering stdin NoBuffering
+    hSetBuffering stdout NoBuffering
+    generatePhase superRS [] where
+      locked :: Bool
+      locked = False
+
+      generatePhase :: RotationSystem -> Playfield -> IO ()
+      generatePhase rs@(RS spawn _ _) pf = mino >>= f rs pf
+        where
+          bag :: IO [Shape]
+          bag = randomGenerate
+          shape :: IO Shape
+          shape = fmap head bag
+          mino :: IO Mino
+          mino = fmap spawn shape
+          f :: RotationSystem -> Playfield -> Mino -> IO ()
+          f x y z = fallingPhase x (y, z)
+
+      fallingPhase :: RotationSystem -> (Playfield, Mino) -> IO ()
+      fallingPhase rs (pf, mino)
+        | locked = completePhase rs pf
+        | otherwise = f >>= fallingPhase rs
+          where
+            f :: IO (Playfield, Mino)
+            f = getAction >>= doAction rs pf mino
+
+      completePhase :: RotationSystem -> Playfield -> IO ()
+      completePhase = generatePhase  --  printField (lineClearGravity $ clearLine pf $ findFullLine pf) >>=
+      
