48835c0ebcd43af431a6c744f956ec3755753c3a
CoNsTaRs <constarwu@gmail.com>
2018-04-04 20:04:58 -0400

    update Playfield data type

--- Playfield.hs
+++ Playfield.hs
@@ -1,7 +1,20 @@
 module Playfield where
 
+  import Tetromino
+
+  -- A Cell is represented by it's Coord and Mino type
+  type Cell = (Int, Int, Shape)
+
   -- A Playfield is represented by a list of cells that is taken
-  type Playfield = [(Int, Int)]
+  type Playfield = [Cell]
+
+  -- Transform to Coords for better usage
+  coord :: Playfield -> [Coord]
+  coord = map (\(x, y, _) -> (x, y))
+
+  -- Land a Mino
+  appendMino :: Playfield -> Mino -> Playfield
+  appendMino pf (Mino xs s _) = pf ++ map (\(x, y) -> (x, y, s)) xs
 
   -- The width of the playfield, left to 0 and right to 9
   fieldWidth :: Int
